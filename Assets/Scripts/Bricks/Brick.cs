3407db68cb51d1448e5834368f37ae8c27e9c6b8
Gustavo Gomes <gustavo@spigo.net>
2020-06-27 16:02:46 -0300

    Rapido

--- Assets/Scripts/Bricks/Brick.cs
+++ Assets/Scripts/Bricks/Brick.cs
@@ -2,6 +2,8 @@
 
 public class Brick : MonoBehaviour
 {
+    public float m_axisY;
+
     public float m_BounceTime = 0.3f;
     public float m_BounceDistance = 0.3f;
     public AnimationCurve m_Curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.5f, 1), new Keyframe(1, 0));
@@ -12,13 +14,22 @@ public class Brick : MonoBehaviour
 
     public AudioClip m_BounceAudioClip;
 
+    private Animator m_anim;
+
     private void Start()
     {
         m_Source = transform.position;
         m_Bounds = GetComponent<Collider>().bounds;
+        m_anim = GetComponent<Animator>();
     }
 
-    private void LateUpdate()
+    private void Update()
+    {
+        transform.position = new Vector3(m_Source.x,
+                                         m_Source.y + m_axisY,
+                                         m_Source.z);
+    }
+    /*private void LateUpdate()
     {
         if (!m_CanBounce) return;
 
@@ -26,7 +37,7 @@ public class Brick : MonoBehaviour
         transform.position = m_Source + Vector3.up * m_BounceDistance * m_Curve.Evaluate(time);
 
         m_CanBounce = time < 1.0f;
-    }
+    }*/
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -34,8 +45,9 @@ public class Brick : MonoBehaviour
         var isBelowBrick = impact.y <= m_Bounds.min.y;
         if (!isBelowBrick) return;
 
-        m_CanBounce = true;
-        m_StartTime = Time.time;
+        m_anim.SetTrigger("Vai");
+        /*m_CanBounce = true;
+        m_StartTime = Time.time;*/
     }
 }
 
